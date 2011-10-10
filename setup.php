2dd6c0ef430a2dddbd850432628ccf0cc2e1b4fb
Larry Adams <thewitness@cacti.net>
2011-10-10 20:56:01 +0000

    Support for Ugroup Plugin
    git-svn-id: svn+ssh://repo.cacti.net/var/data/svnroot/cacti_plugins/cycle/trunk@1847 67214365-cc29-0410-bfbe-843132f0f3d7

--- setup.php
+++ setup.php
@@ -123,7 +123,7 @@ function cycle_setup_table_new () {
 function cycle_version () {
 	return array(
 		'name'     => 'Cycle Graphs',
-		'version'  => '2.0',
+		'version'  => '2.1',
 		'longname' => 'Cycle Graphs',
 		'author'   => 'The Cacti Group',
 		'homepage' => 'http://www.cacti.net',
