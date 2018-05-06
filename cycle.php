b43af6ce3e0452a5a14fd7542e2cff6cdee21d8d
Jimmy Conner <jimmy@sqmail.org>
2018-05-06 13:17:43 -0500

    Resolve Possible Issue Realted to Cacti Issue #1602

--- cycle.php
+++ cycle.php
@@ -31,6 +31,7 @@ include_once('./lib/time.php');
 include_once('./lib/html_tree.php');
 include_once('./lib/api_graph.php');
 include_once('./lib/api_tree.php');
+include_once('./lib/utility.php');
 include_once('./lib/api_data_source.php');
 include_once('./plugins/cycle/functions.php');
 include_once('./plugins/cycle/functions.php');
