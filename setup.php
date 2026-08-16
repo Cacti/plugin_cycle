6bb8b276c7efc0cf8c28b17a8ebeb16d23f81921
Thomas Vincent <thomasvincent@gmail.com>
2026-08-16 09:04:58 -0700

    hardening: prepared statements, PHP 7.4 idioms, and security fixes (#27)
    * refactor(php74): use null coalescing (??) and ??= operators
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>
    
    * chore: add copilot instructions and CI workflow
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>
    
    * style: align plugin with Cacti PHP coding standard
    
    * ci: remove unavailable versioned Apache PHP package
    
    ---------
    
    Signed-off-by: Thomas Vincent <thomasvincent@gmail.com>

--- setup.php
+++ setup.php
@@ -33,30 +33,33 @@ function plugin_cycle_install() {
 
 	api_plugin_register_realm('cycle', 'cycle.php,cycle_ajax.php', __('Plugin -> Cycle Graphs', 'cycle'), 1);
 
-	cycle_setup_table_new ();
+	cycle_setup_table_new();
 }
 
-function plugin_cycle_uninstall () {
-	/* Do any extra Uninstall stuff here */
+function plugin_cycle_uninstall() {
+	// Do any extra Uninstall stuff here
 }
 
-function plugin_cycle_check_config () {
-	/* Here we will check to ensure everything is configured */
+function plugin_cycle_check_config() {
+	// Here we will check to ensure everything is configured
 	cycle_check_upgrade();
+
 	return true;
 }
 
-function plugin_cycle_upgrade () {
-	/* Here we will upgrade to the newest version */
+function plugin_cycle_upgrade() {
+	// Here we will upgrade to the newest version
 	cycle_check_upgrade();
+
 	return false;
 }
 
-function cycle_check_upgrade () {
+function cycle_check_upgrade() {
 	global $config;
 
-	$files = array('index.php', 'plugins.php', 'cycle.php');
-	if (isset($_SERVER['PHP_SELF']) && !in_array(basename($_SERVER['PHP_SELF']), $files)) {
+	$files = ['index.php', 'plugins.php', 'cycle.php'];
+
+	if (isset($_SERVER['PHP_SELF']) && !in_array(basename($_SERVER['PHP_SELF']), $files, true)) {
 		return;
 	}
 
@@ -65,23 +68,24 @@ function cycle_check_upgrade () {
 	$old     = db_fetch_row("SELECT * FROM plugin_config WHERE directory='cycle'");
 
 	if (cacti_sizeof($old) && $current != $old['version']) {
-		/* if the plugin is installed and/or active */
+		// if the plugin is installed and/or active
 		if ($old['status'] == 1 || $old['status'] == 4) {
-			/* re-register the hooks */
+			// re-register the hooks
 			plugin_cycle_install();
 
-			/* perform a database upgrade */
+			// perform a database upgrade
 			cycle_database_upgrade();
 		}
 
 		if ($old < '1.0') {
 			api_plugin_register_realm('cycle', 'cycle.php,cycle_ajax.php', 'Plugin -> Cycle Graphs', 1);
 
-			/* get the realm id's and change from old to new */
-			$user  = db_fetch_cell("SELECT id FROM plugin_realms WHERE file='cycle.php'")+100;
+			// get the realm id's and change from old to new
+			$user  = db_fetch_cell("SELECT id FROM plugin_realms WHERE file='cycle.php'") + 100;
 			$users = db_fetch_assoc('SELECT user_id FROM user_auth_realm WHERE realm_id=42');
+
 			if (sizeof($users)) {
-				foreach($users as $u) {
+				foreach ($users as $u) {
 					db_execute('INSERT INTO user_auth_realm
 						(realm_id, user_id) VALUES (' . $user . ', ' . $u['user_id'] . ')
 						ON DUPLICATE KEY UPDATE realm_id=VALUES(realm_id)');
@@ -92,20 +96,20 @@ function cycle_check_upgrade () {
 			}
 		}
 
-		/* update the plugin information */
+		// update the plugin information
 		$id = db_fetch_cell("SELECT id FROM plugin_config WHERE directory='cycle'");
 
-		/* remove legacy hook */
+		// remove legacy hook
 		db_execute('DELETE FROM plugin_hooks WHERE name="cycle" AND hook="config_form"');
 
 		db_execute_prepared('UPDATE plugin_config
 			SET name = ?, author = ?, webpage = ?, version = ?
 			WHERE id = ?',
-			array($info['longname'], $info['author'], $info['homepage'], $info['version'], $id));
+			[$info['longname'], $info['author'], $info['homepage'], $info['version'], $id]);
 	}
 }
 
-function cycle_database_upgrade () {
+function cycle_database_upgrade() {
 }
 
 function cycle_check_dependencies() {
@@ -114,159 +118,161 @@ function cycle_check_dependencies() {
 	return true;
 }
 
-function cycle_setup_table_new () {
+function cycle_setup_table_new() {
 }
 
-function plugin_cycle_version () {
+function plugin_cycle_version() {
 	global $config;
 	$info = parse_ini_file($config['base_path'] . '/plugins/cycle/INFO', true);
+
 	return $info['info'];
 }
 
-function cycle_page_head () {
+function cycle_page_head() {
 }
 
-function cycle_config_settings ($force = false) {
+function cycle_config_settings($force = false) {
 	global $tabs, $settings, $tabs_graphs, $settings_user, $page_refresh_interval, $graph_timespans;
 	global $cycle_width, $cycle_height, $cycle_cols, $cycle_graphs;
 
-	/* check for an upgrade */
+	// check for an upgrade
 	plugin_cycle_check_config();
 
 	if ($force === false && isset($_SERVER['PHP_SELF']) &&
 		basename($_SERVER['PHP_SELF']) != 'settings.php' &&
-		basename($_SERVER['PHP_SELF']) != 'auth_profile.php')
+		basename($_SERVER['PHP_SELF']) != 'auth_profile.php') {
 		return;
+	}
 
-	$tabs['cycle'] = __('Cycle', 'cycle');
+	$tabs['cycle']        = __('Cycle', 'cycle');
 	$tabs_graphs['cycle'] = __('Cycle', 'cycle');
 
-	$treeList = array_rekey(get_allowed_trees(), 'id', 'name');
-	$tempHeader = array('cycle_header' => array(
+	$treeList   = array_rekey(get_allowed_trees(), 'id', 'name');
+	$tempHeader = ['cycle_header' => [
 			'friendly_name' => __('Cycle Graphs', 'cycle'),
-			'method' => 'spacer',
-			));
-	$temp = array(
-		'cycle_delay' => array(
+			'method'        => 'spacer',
+			]];
+	$temp = [
+		'cycle_delay' => [
 			'friendly_name' => __('Delay Interval', 'cycle'),
-			'description' => __('This is the time in seconds before the next graph is displayed.', 'cycle'),
-			'method' => 'drop_array',
-			'default' => 60,
-			'array' => $page_refresh_interval
-			),
-		'cycle_timespan' => array(
+			'description'   => __('This is the time in seconds before the next graph is displayed.', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => 60,
+			'array'         => $page_refresh_interval
+			],
+		'cycle_timespan' => [
 			'friendly_name' => __('Graph Timespan', 'cycle'),
-			'description' => __('This is the default timespan that will be displayed on the page.', 'cycle'),
-			'method' => 'drop_array',
-			'default' => 5,
-			'array' => $graph_timespans
-			),
-		'cycle_columns' => array(
+			'description'   => __('This is the default timespan that will be displayed on the page.', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => 5,
+			'array'         => $graph_timespans
+			],
+		'cycle_columns' => [
 			'friendly_name' => __('Column Count', 'cycle'),
-			'description' => __('In Tree Mode this is the number of columns that will be used.', 'cycle'),
-			'method' => 'drop_array',
-			'default' => 2,
-			'array' => $cycle_cols
-			),
-		'cycle_graphs' => array(
+			'description'   => __('In Tree Mode this is the number of columns that will be used.', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => 2,
+			'array'         => $cycle_cols
+			],
+		'cycle_graphs' => [
 			'friendly_name' => __('Number of Graphs per Page', 'cycle'),
-			'description' => __('Select the number of graphs to display per page', 'cycle'),
-			'method' => 'drop_array',
-			'default' => '4',
-			'array' => $cycle_graphs,
-			),
-		'cycle_height' => array(
+			'description'   => __('Select the number of graphs to display per page', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => '4',
+			'array'         => $cycle_graphs,
+			],
+		'cycle_height' => [
 			'friendly_name' => __('Graph Height', 'cycle'),
-			'description' => __('This sets the graph height for the displayed graphs.', 'cycle'),
-			'method' => 'drop_array',
-			'default' => '100',
-			'array' => $cycle_height
-			),
-		'cycle_width' => array(
+			'description'   => __('This sets the graph height for the displayed graphs.', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => '100',
+			'array'         => $cycle_height
+			],
+		'cycle_width' => [
 			'friendly_name' => __('Graph Width', 'cycle'),
-			'description' => __('This sets the graph width for the displayed graphs.', 'cycle'),
-			'method' => 'drop_array',
-			'default' => '400',
-			'array' => $cycle_width
-			),
-		'cycle_legend' => array(
+			'description'   => __('This sets the graph width for the displayed graphs.', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => '400',
+			'array'         => $cycle_width
+			],
+		'cycle_legend' => [
 			'friendly_name' => __('Display Legend', 'cycle'),
-			'description' => __('Check this to display legend.', 'cycle'),
-			'method' => 'checkbox',
-			'default' => ''
-			),
-		'cycle_cheader' => array(
+			'description'   => __('Check this to display legend.', 'cycle'),
+			'method'        => 'checkbox',
+			'default'       => ''
+			],
+		'cycle_cheader' => [
 			'friendly_name' => __('Predefined Rotations', 'cycle'),
-			'method' => 'spacer',
-			),
-		'cycle_custom_graphs_type' => array(
+			'method'        => 'spacer',
+			],
+		'cycle_custom_graphs_type' => [
 			'friendly_name' => __('Rotation Type', 'cycle'),
-			'description' => __('Select which method to use for custom graph rotation.  If you select \'Specific List\', you must define a list of Graph ID\'s', 'cycle'),
-			'method' => 'drop_array',
-			'default' => '1',
-			'array' => array(0 => __('Legacy (All)', 'cycle'), 1 => __('Specific List', 'cycle'), 2 => __('Tree Mode', 'cycle')),
-			),
-		'cycle_custom_graphs_list' => array(
+			'description'   => __('Select which method to use for custom graph rotation.  If you select \'Specific List\', you must define a list of Graph ID\'s', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => '1',
+			'array'         => [0 => __('Legacy (All)', 'cycle'), 1 => __('Specific List', 'cycle'), 2 => __('Tree Mode', 'cycle')],
+			],
+		'cycle_custom_graphs_list' => [
 			'friendly_name' => __('Custom Graph List', 'cycle'),
-			'description' => __('This must be a comma delimited list of Graph ID\'s to cycle through. For example \'1,2,3,4\'', 'cycle'),
-			'method' => 'textbox',
-			'max_length' => 255,
-			),
-		'cycle_custom_graphs_tree' => array(
+			'description'   => __('This must be a comma delimited list of Graph ID\'s to cycle through. For example \'1,2,3,4\'', 'cycle'),
+			'method'        => 'textbox',
+			'max_length'    => 255,
+			],
+		'cycle_custom_graphs_tree' => [
 			'friendly_name' => __('Default Tree', 'cycle'),
-			'description' => __('Select the graph tree to cycle if Tree Mode is selected', 'cycle'),
-			'method' => 'drop_array',
-			'default' => 'None',
-			'array' => $treeList,
-		)
-	);
+			'description'   => __('Select the graph tree to cycle if Tree Mode is selected', 'cycle'),
+			'method'        => 'drop_array',
+			'default'       => 'None',
+			'array'         => $treeList,
+		]
+	];
 
 	if (isset($settings['cycle'])) {
 		$settings['cycle'] = array_merge($settings['cycle'], $tempHeader, $temp);
-	}else {
+	} else {
 		$settings['cycle'] = array_merge($tempHeader, $temp);
 	}
 
 	if (isset($settings_user['cycle'])) {
 		$settings_user['cycle'] = array_merge($settings_user['cycle'], $temp);
-	}else {
+	} else {
 		$settings_user['cycle'] = $temp;
 	}
 }
 
-function cycle_show_tab () {
+function cycle_show_tab() {
 	global $config;
 
 	if (api_user_realm_auth('cycle.php')) {
 		if (substr_count($_SERVER['REQUEST_URI'], 'cycle.php')) {
 			print '<a href="' . $config['url_path'] . 'plugins/cycle/cycle.php"><img src="' . $config['url_path'] . 'plugins/cycle/images/tab_cycle_down.gif" alt="' . __('Cycle') . '"></a>';
-		}else{
+		} else {
 			print '<a href="' . $config['url_path'] . 'plugins/cycle/cycle.php"><img src="' . $config['url_path'] . 'plugins/cycle/images/tab_cycle.gif" alt="' . __('Cycle') . '"></a>';
 		}
 	}
 }
 
-function cycle_config_arrays () {
+function cycle_config_arrays() {
 	global $cycle_graphs, $cycle_cols, $cycle_width, $cycle_height;
 
-	$cycle_graphs = array(
+	$cycle_graphs = [
 		1  => __('%d Graph', 1, 'cycle'),
 		2  => __('%d Graphs', 2, 'cycle'),
 		4  => __('%d Graphs', 4, 'cycle'),
 		6  => __('%d Graphs', 6, 'cycle'),
 		8  => __('%d Graphs', 8, 'cycle'),
 		10 => __('%d Graphs', 10, 'cycle')
-	);
+	];
 
-	$cycle_cols   = array(
+	$cycle_cols   = [
 		1 => __('%d Column', 1, 'cycle'),
 		2 => __('%d Columns', 2, 'cycle'),
 		3 => __('%d Columns', 3, 'cycle'),
 		4 => __('%d Columns', 4, 'cycle'),
 		5 => __('%d Columns', 5, 'cycle')
-	);
+	];
 
-	$cycle_height = array(
+	$cycle_height = [
 		75  => __('%d Pixels', 75, 'cycle'),
 		100 => __('%d Pixels', 100, 'cycle'),
 		125 => __('%d Pixels', 125, 'cycle'),
@@ -278,9 +284,9 @@ function cycle_config_arrays () {
 		350 => __('%d Pixels', 350, 'cycle'),
 		400 => __('%d Pixels', 400, 'cycle'),
 		500 => __('%d Pixels', 500, 'cycle')
-	);
+	];
 
-	$cycle_width  = array(
+	$cycle_width  = [
 		100 => __('%d Pixels', 100, 'cycle'),
 		125 => __('%d Pixels', 125, 'cycle'),
 		150 => __('%d Pixels', 150, 'cycle'),
@@ -295,19 +301,20 @@ function cycle_config_arrays () {
 		600 => __('%d Pixels', 600, 'cycle'),
 		650 => __('%d Pixels', 650, 'cycle'),
 		700 => __('%d Pixels', 700, 'cycle')
-	);
+	];
 
 	return true;
 }
 
-function cycle_draw_navigation_text ($nav) {
-	$nav['cycle.php:'] = array('title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1');
-	$nav['cycle.php:view'] = array('title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1');
-	$nav['cycle.php:graphs'] = array('title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1');
-	$nav['cycle.php:save'] = array('title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1');
+function cycle_draw_navigation_text($nav) {
+	$nav['cycle.php:']       = ['title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1'];
+	$nav['cycle.php:view']   = ['title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1'];
+	$nav['cycle.php:graphs'] = ['title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1'];
+	$nav['cycle.php:save']   = ['title' => __('Cycling', 'cycle'), 'mapping' => '', 'url' => 'cycle.php', 'level' => '1'];
+
 	return $nav;
 }
 
-function cycle_api_graph_save ($save) {
+function cycle_api_graph_save($save) {
 }
 ?>
